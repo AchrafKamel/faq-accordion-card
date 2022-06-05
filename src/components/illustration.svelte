16f373fbc29730cdc9dde43d0cc1035d6b721dc0
AchrafKamel <achrafkamel7@gmail.com>
2022-06-05 02:37:43 +0100

    fix background image import

--- src/components/illustration.svelte
+++ src/components/illustration.svelte
@@ -7,7 +7,7 @@
 	<img class="w-64 max-w-xs mx-auto" src="bg-pattern-mobile.svg" alt="bg pattern" />
 </div>
 
-<div class="hidden md:block md:w-1/2 bg-[url('stacked.svg')] bg-contain bg-no-repeat" >
+<div class="hidden md:block md:w-1/2 bg-[url('/stacked.svg')] bg-contain bg-no-repeat" >
 	<img
 		class="relative w-1/3 top-1/2 -translate-y-1/2 -translate-x-1/2"
 		src="illustration-box-desktop.svg"
